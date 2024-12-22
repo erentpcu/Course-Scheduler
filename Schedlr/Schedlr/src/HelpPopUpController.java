8ad59689fcf35a7f215f6bc786dda7984adf56eb
hilalsinem <hilalsinemsayar@gmail.com>
2024-12-22 18:57:28 +0300

    Help and Documentation & Contributors added

---
+++ Schedlr/Schedlr/src/HelpPopUpController.java
@@ -0,0 +1,14 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
+
+public class HelpPopUpController {
+
+    @FXML
+    private TabPane tabPane;
+
+    /**
+     * Handles the action of closing the Help window.
+     */
+
+}
